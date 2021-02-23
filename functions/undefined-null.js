7716e3244db6986a28bedc3a19762f0178726964
shivani brijmohan <shivani.brijmohan@gmail.com>
2021-02-23 11:59:02 -0500

    undefined vs null fundamentals

---
+++ functions/undefined-null.js
@@ -0,0 +1,25 @@
+// undefined represents the absence of a value
+
+// undefined for variable
+let name
+if (name === undefined) {
+  console.log('Please provide a name');
+}else{
+  console.log(name);
+}
+
+// undefined for function arguments
+// undefined as the function returned default value
+let square = function(num){
+  console.log(num);
+};
+
+let result = square();
+console.log(result);
+square(3);
+
+// Null is meant to clear out something
+// explicitly done by thee developer
+let age = 28;
+age = null;
+console.log(age);
